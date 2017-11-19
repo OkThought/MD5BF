019ba63fb9c9ac8f1b87161f3565559b84d24857
RoyPy <bogush.vano@gmail.com>
2017-11-20 01:34:45 +0700

    Add hash to task

--- src/ru/ccfit/nsu/bogush/md5bf/Task.java
+++ src/ru/ccfit/nsu/bogush/md5bf/Task.java
@@ -7,16 +7,20 @@ import java.io.Serializable;
 public class Task implements Serializable {
     long sequenceStartIndex;
     long sequenceFinishIndex;
+    byte[] hash;
     String alphabet;
 
-    public Task(long sequenceStartIndex, long sequenceFinishIndex, String alphabet) {
+    public Task(long sequenceStartIndex, long sequenceFinishIndex, byte[] hash, String alphabet) {
         this.sequenceStartIndex = sequenceStartIndex;
         this.sequenceFinishIndex = sequenceFinishIndex;
+        this.hash = hash;
         this.alphabet = alphabet;
     }
 
     private void writeObject(java.io.ObjectOutputStream out)
             throws IOException {
+        out.writeInt(hash.length);
+        out.write(hash);
         out.writeInt(alphabet.length());
         out.writeChars(alphabet);
         out.writeLong(sequenceStartIndex);
@@ -25,10 +29,15 @@ public class Task implements Serializable {
 
     private void readObject(java.io.ObjectInputStream in)
             throws IOException, ClassNotFoundException {
+        hash = new byte[in.readInt()];
+        in.readFully(hash);
+
         char[] alphabetChars = new char[in.readInt()];
         for (int i = 0; i < alphabetChars.length; ++i) {
             alphabetChars[i] = in.readChar();
         }
+        this.alphabet = new String(alphabetChars);
+
         sequenceStartIndex = in.readLong();
         sequenceFinishIndex = in.readLong();
     }
