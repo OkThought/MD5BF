32f9d004807969663c4d2508026c667173e68bf9
RoyPy <bogush.vano@gmail.com>
2017-11-19 22:30:58 +0700

    Add ALPHABET

--- src/ru/ccfit/nsu/bogush/md5bf/MD5BFInfo.java
+++ src/ru/ccfit/nsu/bogush/md5bf/MD5BFInfo.java
@@ -5,5 +5,5 @@ import java.nio.charset.Charset;
 abstract class MD5BFInfo {
     static final String PROTOCOL = "MD5BF_IB";
     static final Charset CHARSET = Charset.forName("UTF-8");
-
+    static final String ALPHABET = "ACGT";
 }
