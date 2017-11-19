471a4e7a27a93dd1fe567eb8c69b0f5a722e0a73
RoyPy <bogush.vano@gmail.com>
2017-11-19 19:33:50 +0700

    Implement SymbolSequenceCalculator

---
+++ src/ru/ccfit/nsu/bogush/md5bf/SymbolSequenceCalculator.java
@@ -0,0 +1,68 @@
+package ru.ccfit.nsu.bogush.md5bf;
+
+import java.util.Arrays;
+
+public class SymbolSequenceCalculator {
+    public static long pow(int a, int b) {
+        if (a == 0) return 0;
+        if (b == 0) return 1;
+        if (b == 1) return a;
+        if (b % 2 == 0) return pow(a*a, b/2);
+        return a * pow(a*a, b/2);
+    }
+
+    public static long numberOfSequences(int sequenceLength, int alphabetSize) {
+        long numberOfSequences = 1;
+        for (int i = 1; i <= sequenceLength; ++i) {
+            numberOfSequences += pow(alphabetSize, i);
+        }
+        return numberOfSequences;
+    }
+
+    public static int sequenceLength(long sequenceIndex, int alphabetSize) {
+        if (sequenceIndex == 0) return 0;
+        if (sequenceIndex <= alphabetSize) return 1;
+        int lenght1 = (int) Math.floor(Math.log(sequenceIndex) / Math.log(alphabetSize));
+        int lenght2 = lenght1+1;
+        if (sequenceIndex < numberOfSequences(lenght1, alphabetSize)) {
+            return lenght1;
+        } else {
+            return lenght2;
+        }
+    }
+
+    public static int[] sequence(long sequenceIndex, int alphabetSize) {
+        int length = sequenceLength(sequenceIndex, alphabetSize);
+        int[] sequence = new int[length];
+        if (length == 0) return sequence;
+        long lastIndexOfPreviousLength = numberOfSequences(length-1, alphabetSize);
+        long cur = sequenceIndex - lastIndexOfPreviousLength;
+        long next;
+        for (int i = length-1; i >= 0; --i) {
+            next = cur / alphabetSize;
+            int rest = (int) (cur % alphabetSize);
+            sequence[i] = rest;
+            cur = next;
+        }
+        return sequence;
+    }
+
+    public static String stringFromSequenceIndex(long sequenceIndex, String alphabet) {
+        int[] sequence = sequence(sequenceIndex, alphabet.length());
+        char[] chars = new char[sequence.length];
+        for (int i = 0; i < sequence.length; i++) {
+            chars[i] = alphabet.charAt(sequence[i]);
+        }
+        return new String(chars);
+    }
+
+    public static long sequenceIndex(String sequence, String alphabet) {
+        if (sequence.length() == 0) return 0;
+        long sequenceIndex = numberOfSequences(sequence.length()-1, alphabet.length());
+        char[] chars = sequence.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            sequenceIndex += pow(alphabet.length(), chars.length-i-1) * alphabet.indexOf(chars[i]);
+        }
+        return sequenceIndex;
+    }
+}
