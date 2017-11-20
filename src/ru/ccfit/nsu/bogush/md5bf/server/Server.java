11326cab7d9833217ee8e9a66959dbbc033ddd8f
RoyPy <bogush.vano@gmail.com>
2017-11-20 16:33:33 +0700

    Fix server

--- src/ru/ccfit/nsu/bogush/md5bf/server/Server.java
+++ src/ru/ccfit/nsu/bogush/md5bf/server/Server.java
@@ -3,9 +3,8 @@ package ru.ccfit.nsu.bogush.md5bf.server;
 import ru.ccfit.nsu.bogush.md5bf.ConnectionRequestType;
 import ru.ccfit.nsu.bogush.md5bf.bf.Task;
 import ru.ccfit.nsu.bogush.md5bf.bf.TaskCreator;
-import ru.ccfit.nsu.bogush.md5bf.net.SocketReader;
-import ru.ccfit.nsu.bogush.md5bf.net.SocketWriter;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -18,33 +17,34 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static ru.ccfit.nsu.bogush.md5bf.MD5BFInfo.ALPHABET;
-import static ru.ccfit.nsu.bogush.md5bf.MD5BFInfo.CHARSET;
 import static ru.ccfit.nsu.bogush.md5bf.MD5BFInfo.PROTOCOL;
 
-public class Server extends Thread {
+public class Server extends Thread implements TaskCreator.TaskCreatorListener {
     private static final int EXIT_FAILURE = 1;
     private static final int TASK_QUEUE_SIZE = 16;
-    private static final int MAX_SEQUENCE_LENGTH = 16;
+    private static final int MAX_SEQUENCE_LENGTH = 5;
     private static final long INDEX_STEP = 1024;
     private static final int REQUIRED_NUMBER_OF_ARGUMENTS = 2;
     private static final int HASH_ARGUMENT_INDEX = 0;
     private static final int PORT_ARGUMENT_INDEX = 1;
     private static final long TASK_TIMEOUT = 1000; // millis
     private static final int ACCEPT_TIMEOUT = 5000; // millis
+    private final LinkedBlockingQueue<Task> taskQueue = new LinkedBlockingQueue<>(TASK_QUEUE_SIZE);
     private ServerSocket serverSocket;
     private TaskCreator taskCreator;
-    private LinkedBlockingQueue<Task> taskQueue = new LinkedBlockingQueue<>(TASK_QUEUE_SIZE);
     private ConcurrentHashMap<UUID, AssignedTask> assignedTasks = new ConcurrentHashMap<>();
-    private TaskTimeoutGuard taskTimeoutGuard = new TaskTimeoutGuard();
     private int serverPort;
+    private boolean tasksFinished = false;
 
-    public Server(String md5hash, int port) throws IOException {
+    private Server(String md5hashString, int port) throws IOException {
         super("Server");
-        System.err.println("Initialize MD5 Brute-Force Server of the hash " + md5hash);
+        System.err.println("Initialize MD5 Brute-Force Server of the hash " + md5hashString);
         serverPort = port;
         this.serverSocket = new ServerSocket();
         serverSocket.setSoTimeout(ACCEPT_TIMEOUT);
-        taskCreator = new TaskCreator(taskQueue, INDEX_STEP, MAX_SEQUENCE_LENGTH, md5hash.getBytes(CHARSET), ALPHABET);
+        byte[] md5hash = DatatypeConverter.parseHexBinary(md5hashString);
+        taskCreator = new TaskCreator(taskQueue, INDEX_STEP, MAX_SEQUENCE_LENGTH, md5hash, ALPHABET);
+        taskCreator.setTaskCreatorListener(this);
     }
 
     private static void usage() {
@@ -98,104 +98,106 @@ public class Server extends Thread {
     @Override
     public void run() {
         while (!Thread.interrupted()) {
-            Socket clientSocket;
+            Socket clientSocket = null;
             try {
                 System.err.println("Listen to incoming connections");
                 clientSocket = serverSocket.accept();
-
             } catch (SocketTimeoutException e) {
                 System.err.println("Listen timed out");
-                taskTimeoutGuard.run();
-                continue;
             } catch (IOException e) {
                 System.err.println("Server: An I/O error occurs when waiting for a connection.");
                 continue;
             }
 
-//            SocketReader socketReader;
-//            SocketWriter socketWriter;
-
-            ObjectInputStream in;
-            ObjectOutputStream out;
-            try {
-//                socketReader = new SocketReader(clientSocket.getInputStream());
-//                socketWriter = new SocketWriter(clientSocket.getOutputStream());
-                out = new ObjectOutputStream(clientSocket.getOutputStream());
-                in = new ObjectInputStream(clientSocket.getInputStream());
-            } catch (IOException e) {
-                System.err.println("Couldn't get i/o streams of the client socket");
-                try {
-                    clientSocket.close();
-                } catch (IOException e1) {
-                    System.err.println("FATAL: Couldn't close client socket");
-                    System.exit(EXIT_FAILURE);
-                }
-                continue;
+            if (clientSocket != null) {
+                handleConnection(clientSocket);
             }
 
-            System.err.println("Client connecting");
+            moveToTaskQueueTimedOutTasks();
 
-            if (!recognizeClientProtocol(in)) {
-                try {
-                    clientSocket.close();
-                } catch (IOException e) {
-                    System.err.println("Couldn't close client socket");
-                }
-                continue;
+            if (tasksFinished && assignedTasks.isEmpty() && taskQueue.isEmpty()) {
+                break;
             }
-
-            UUID uuid;
-            try {
-                uuid = (UUID) in.readObject();
-                System.err.println("Received uuid " + uuid);
-            } catch (IOException | ClassNotFoundException e) {
-                System.err.println("Couldn't read uuid");
-                e.printStackTrace();
-                continue;
-            }
-
-            ConnectionRequestType type;
-            try {
-                type = ConnectionRequestType.forByte(in.readByte());
-                System.err.println("Received connection request type " + type);
-            } catch (IOException e) {
-                System.err.println("Couldn't read connection request type");
-                e.printStackTrace();
-                try {
-                    clientSocket.close();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                continue;
-            }
-
-            switch (type) {
-                case TASK_REQUEST:
-                    System.err.println("Received TASK_REQUEST from client " + uuid);
-                    handleTaskRequest(out, uuid);
-                    break;
-                case TASK_DONE:
-                    System.err.println("Received TASK_DONE from client " + uuid);
-                    handleTaskDone(in);
-                    break;
-                default:
-                    System.err.println("Unknown client connection request type detected");
-                    return;
-            }
-
-            try {
-                clientSocket.getOutputStream().flush();
-                clientSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            taskTimeoutGuard.run();
         }
         try {
             serverSocket.close();
         } catch (IOException e) {
             System.err.println("Couldn't close server socket");
+            e.printStackTrace();
+        }
+    }
+
+    private void handleConnection(Socket clientSocket) {
+        ObjectInputStream in;
+        ObjectOutputStream out;
+        try {
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            in = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+            System.err.println("Couldn't get i/o streams of the client socket");
+            try {
+                clientSocket.close();
+            } catch (IOException e1) {
+                System.err.println("FATAL: Couldn't close client socket");
+                System.exit(EXIT_FAILURE);
+            }
+            return;
+        }
+
+        System.err.println("Client connecting");
+
+        if (!recognizeClientProtocol(in)) {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.err.println("Couldn't close client socket");
+            }
+            return;
+        }
+
+        UUID uuid;
+        try {
+            uuid = (UUID) in.readObject();
+            System.err.println("Received uuid " + uuid);
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Couldn't read uuid");
+            return;
+        }
+
+        ConnectionRequestType type;
+        try {
+            type = ConnectionRequestType.forByte(in.readByte());
+            System.err.println("Received connection request type " + type);
+        } catch (IOException e) {
+            System.err.println("Couldn't read connection request type");
+            try {
+                clientSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return;
+        }
+
+        switch (type) {
+            case TASK_REQUEST:
+                System.err.println("Received TASK_REQUEST from client " + uuid);
+                handleTaskRequest(clientSocket, out, uuid);
+                break;
+            case TASK_DONE:
+                System.err.println("Received TASK_DONE from client " + uuid);
+                handleTaskDone(in);
+                break;
+            default:
+                System.err.println("Unknown client connection request type detected");
+                return;
+        }
+
+        try {
+            if (!clientSocket.isClosed()) {
+                clientSocket.getOutputStream().flush();
+                clientSocket.close();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -215,8 +217,19 @@ public class Server extends Thread {
         return true;
     }
 
-    private void handleTaskRequest(ObjectOutputStream out, UUID uuid) {
-        Task task = null;
+    private void handleTaskRequest(Socket clientSocket, ObjectOutputStream out, UUID uuid) {
+        if (tasksFinished && taskQueue.isEmpty()) {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Brute-force finished without results");
+            interrupt();
+            return;
+        }
+
+        Task task;
         try {
             task = taskQueue.take();
         } catch (InterruptedException e) {
@@ -249,14 +262,36 @@ public class Server extends Thread {
             System.exit(EXIT_FAILURE);
         }
 
-        System.out.println(secretString);
+        System.out.println("secret string is: \"" + secretString + '"');
         interrupt();
+    }
+
+    private void moveToTaskQueueTimedOutTasks() {
+        assignedTasks.forEach((uuid, assignedTask) -> {
+            if (System.currentTimeMillis() > assignedTask.timeLimit) {
+                assignedTasks.remove(uuid);
+                System.err.println("Client " + uuid + " task timed out");
+                try {
+                    taskQueue.put(assignedTask.task);
+                } catch (InterruptedException e) {
+                    System.err.println(getName() + ": Interrupted on task put in queue");
+                }
+            }
+        });
     }
 
     @Override
     public void interrupt() {
-        taskCreator.interrupt();
+        if (taskCreator.isAlive()) {
+            taskCreator.interrupt();
+        }
         super.interrupt();
+    }
+
+    @Override
+    public void tasksFinished() {
+        System.err.println("Tasks finished");
+        tasksFinished = true;
     }
 
     private static class AssignedTask {
@@ -266,23 +301,6 @@ public class Server extends Thread {
         private AssignedTask(Task task, long timeLimit) {
             this.task = task;
             this.timeLimit = timeLimit;
-        }
-    }
-
-    private class TaskTimeoutGuard implements Runnable {
-        @Override
-        public void run() {
-            assignedTasks.forEach((uuid, assignedTask) -> {
-                if (System.currentTimeMillis() > assignedTask.timeLimit) {
-                    assignedTasks.remove(uuid);
-                    System.err.println("Client " + uuid + " task timed out");
-                    try {
-                        taskQueue.put(assignedTask.task);
-                    } catch (InterruptedException e) {
-                        System.err.println(getName() + ": Interrupted on task put in queue");
-                    }
-                }
-            });
         }
     }
 }
